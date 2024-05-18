75ef32f1a00775af390a459537ebcce02b7ad863
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2024-05-18 13:02:18 +0330

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,25 +1,5 @@
 { pkgs }: {
   deps = [
-      pkgs.pyrogram
-      pkgs.TgCrypto
-      pkgs.pytgcalls
-      pkgs.python-dotenv
-      pkgs.requests
-      pkgs.aiohttp
-      pkgs.youtube_search_python
-      pkgs.asyncio
-      pkgs.youtube_search
-      pkgs.youtube-dl
-      pkgs.search_engine_parser
-      pkgs.wget
-      pkgs.python-arq
-      pkgs.aiofiles
-      pkgs.ffmpeg
-      pkgs.motor
-      pkgs.dnspython
-      pkgs.bs4
-      pkgs.psutil
-      pkgs.gitpython
 
   ];
 }
